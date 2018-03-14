39e532ae5726f620450a6d0df1c367658bc956c6
Hyunjae Song <shj941013@yahoo.com>
2018-03-14 13:28:17 -0400

    login controller

---
+++ src/main/java/com/Controllers/Login.java
@@ -0,0 +1,17 @@
+package com.Controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+    public class Login {
+        @RequestMapping(value = "/login", method = RequestMethod.POST /*produces = MediaType.APPLICATION_JSON_VALUE*/)
+        public String login() {
+            return "login is working";
+        }
+
+
+}
