4388e59cf661f866eb842f065e25f70779ece4fc
Hyunjae Song <shj941013@yahoo.com>
2018-03-16 09:05:18 -0400

    login sorta done

--- src/main/java/com/Controllers/Login.java
+++ src/main/java/com/Controllers/Login.java
@@ -3,15 +3,20 @@ package com.Controllers;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-    public class Login {
-        @RequestMapping(value = "/login", method = RequestMethod.POST /*produces = MediaType.APPLICATION_JSON_VALUE*/)
-        public String login() {
-            return "login is working";
-        }
+public class Login {
+    @RequestMapping(value = "/login", method = RequestMethod.POST /*produces = MediaType.APPLICATION_JSON_VALUE*/)
+    public String login(@RequestParam("username") String username) {
+        return "login is working";
+    }
 
+    @RequestMapping(value = "/logout")
+    public String logout(@RequestParam("username") String username) {
+        return "logout successful";
+    }
 
 }
