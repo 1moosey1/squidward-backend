c2b9c8ccde1bdb43cf31e6580abd006d91a94d4a
Brooke <bsextion@gmail.com>
2018-03-17 19:59:31 -0400

    Removing errora

--- src/test/java/com/squidward/beans/ProjectServiceTest.java
+++ src/test/java/com/squidward/beans/ProjectServiceTest.java
@@ -3,12 +3,11 @@ package com.squidward.beans;
 import net.sf.beanrunner.BeanRunner;
 import org.junit.Test;
 
-<<<<<<< HEAD:src/test/java/com/squidward/beans/ProjectServiceTest.java
+
 public class ProjectServiceTest {
-=======
+
 public class ProjectTest {
 
->>>>>>> master:src/test/java/com/squidward/beans/ProjectTest.java
     @Test
     public void testProject() throws Exception {
         BeanRunner beanRunner = new BeanRunner();
