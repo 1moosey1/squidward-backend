7c693b7a6703483983cd9419ec92c6df77b43498
Brooke <bsextion@gmail.com>
2018-03-17 20:06:19 -0400

    Updates

---
+++ src/test/java/com/squidward/beans/ProjectTest.java
@@ -0,0 +1,9 @@
+package com.squidward.beans;
+
+import static org.junit.Assert.*;
+
+public class ProjectTest {
+    BeanRunner beanRunner = new BeanRunner();
+    beanRunner.testBean(new Sprint());
+
+}
