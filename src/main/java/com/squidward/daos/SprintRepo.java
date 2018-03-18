2ca05837738291ada57d5db93ea121eb6d8bca53
Brooke <bsextion@gmail.com>
2018-03-17 21:26:04 -0400

    Updates...

--- src/main/java/com/squidward/daos/SprintRepo.java
+++ src/main/java/com/squidward/daos/SprintRepo.java
@@ -4,7 +4,13 @@ import com.squidward.beans.Sprint;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SprintRepo extends CrudRepository<Sprint, Integer> {
+    //Get all Sprints
+    List<Sprint> getAll();
 
+    //Add a Project
+      void addSprint(Sprint sprint);
 }
--- src/main/java/com/squidward/daos/UserStoryRepo.java
+++ src/main/java/com/squidward/daos/UserStoryRepo.java
@@ -4,7 +4,10 @@ import com.squidward.beans.UserStory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserStoryRepo extends CrudRepository<UserStory, Integer> {
 
+    List<UserStory> getById(int id);
 }
