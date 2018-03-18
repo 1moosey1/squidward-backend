8a68e9840276b9e930684e79a39fd53bd81a11ea
1moosey1 <jgonzalez498@gmail.com>
2018-03-18 14:32:25 -0400

    Minor renaming change to GithubConfig

---
+++ src/main/java/com/squidward/utils/GithubConfig.java
@@ -0,0 +1,41 @@
+package com.squidward.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GithubConfig {
+
+    @Value(value="https://github.com/login/oauth/access_token")
+    private String oAuthURI;
+
+    @Value(value="Iv1.d10898f268a22d3b")
+    private String clientId;
+
+    @Value(value="500f9eba1d15af0ef06fdeabd8ce66a00801f733")
+    private String clientSecret;
+
+    public String getOAuthURI() {
+        return oAuthURI;
+    }
+
+    public void setOAuthURI(String oauthURI) {
+        this.oAuthURI = oauthURI;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+}
