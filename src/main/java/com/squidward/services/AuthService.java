5be52bc6c6ca6b4360c3229495708489e3bd02fb
1moosey1 <jgonzalez498@gmail.com>
2018-03-18 23:23:18 -0400

    Backend OAuth authentication completed

--- src/main/java/com/squidward/services/AuthService.java
+++ src/main/java/com/squidward/services/AuthService.java
@@ -1,6 +1,8 @@
 package com.squidward.services;
 
 import com.squidward.utils.GithubConfig;
+import com.squidward.utils.Parameters;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +13,9 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+
+@Slf4j
 @Service
 public class AuthService {
 
@@ -21,25 +26,23 @@ public class AuthService {
         this.githubConfig = githubConfig;
     }
 
-    public String login(String code) {
+    public Parameters login(String code) {
+
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("client_id", githubConfig.getClientId());
-        map.add("client_secret", githubConfig.getClientSecret());
-        map.add("code", code);
+        map.add(githubConfig.getClientIdParam(), githubConfig.getClientId());
+        map.add(githubConfig.getClientSecretParam(), githubConfig.getClientSecret());
+        map.add(githubConfig.getCodeParam(), code);
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
 
-        ResponseEntity<String> response = restTemplate.postForEntity(
-                githubConfig.getOAuthURI(),
-                request,
-                String.class
-        );
+        ResponseEntity<String> response =
+                restTemplate.postForEntity(githubConfig.getOAuthURI(), request, String.class);
 
-        response.getStatusCode();
-        return "login is working" + response.getStatusCode() + response.getBody() + response.getHeaders().toString();
+        response.getHeaders();
+        return new Parameters(response.getBody());
     }
 }
