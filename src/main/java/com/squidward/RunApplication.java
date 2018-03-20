87d08b1b9b3a905d0b5f168ae3975a0601c61afb
1moosey1 <jgonzalez498@gmail.com>
2018-03-19 23:06:59 -0400

    Added CORS Filter

--- src/main/java/com/squidward/RunApplication.java
+++ src/main/java/com/squidward/RunApplication.java
@@ -1,34 +1,17 @@
 package com.squidward;
 
-import com.squidward.filters.AuthFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @SpringBootApplication
+@Configuration
 public class RunApplication {
 
     public static void main(String[] args) {
         log.debug("Starting Spring Application");
         SpringApplication.run(RunApplication.class,args);
-    }
-
-    @Bean
-    public FilterRegistrationBean<AuthFilter> authFilterRegistration() {
-
-        FilterRegistrationBean<AuthFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(authFilter());
-        registration.addUrlPatterns("/api/*");
-        registration.setName("AuthFilter");
-        registration.setOrder(1);
-        return registration;
-    }
-
-    @Bean
-    public AuthFilter authFilter() {
-        return new AuthFilter();
     }
 }
