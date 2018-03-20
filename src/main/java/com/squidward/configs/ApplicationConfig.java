888d4ac929e6f7f88d303b883e5cb293066e051a
1moosey1 <jgonzalez498@gmail.com>
2018-03-20 12:34:40 -0400

    Moved ApplicationConfig and GithubConfig to configs package

---
+++ src/main/java/com/squidward/configs/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.squidward.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class ApplicationConfig {
+
+    @Value("${application.issuer}")
+    private String issuer;
+
+    @Value("${application.token.expiration}")
+    private long tokenExpiration;
+}
