8e8fe762fe0420936e22362e3412bc7f47a3ca6d
1moosey1 <jgonzalez498@gmail.com>
2018-03-21 15:18:26 -0400

    removed unused imports for cookies

--- src/main/java/com/squidward/filters/AuthFilter.java
+++ src/main/java/com/squidward/filters/AuthFilter.java
@@ -13,11 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.util.WebUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
