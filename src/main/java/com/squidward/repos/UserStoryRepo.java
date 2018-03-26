e957aa2a341935776f10ece1cbda22a15c580250
1moosey1 <jgonzalez498@gmail.com>
2018-03-25 20:58:35 -0400

    Added difficulty summary queries to UserStoryRepo and SprintRepo

--- src/main/java/com/squidward/repos/UserStoryRepo.java
+++ src/main/java/com/squidward/repos/UserStoryRepo.java
@@ -1,9 +1,11 @@
 package com.squidward.repos;
 
 import com.squidward.beans.UserStory;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,10 @@ public interface UserStoryRepo extends CrudRepository<UserStory, Integer> {
     Optional<UserStory> findByStartTagAndSprintProjectId(String startTag, int projectId);
     Optional<UserStory> findByDoneTagAndSprintProjectId(String doneTag, int projectId);
     int countBySprintId(int sprintId);
+
+    @Query("select coalesce(sum(difficulty), 0) from UserStory where sprint.id = ?1")
+    int getOverallPointSum(int sprintId);
+
+    @Query("select coalesce(sum(difficulty), 0) from UserStory where sprint.id = ?1 and doneDate = ?2")
+    int getDoneDatePointSum(int sprintId, Date doneDate);
 }
