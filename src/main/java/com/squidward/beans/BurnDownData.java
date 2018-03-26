987a61dd007baa2f56f91adff94a3ee7a1fdad37
1moosey1 <jgonzalez498@gmail.com>
2018-03-25 21:36:56 -0400

    Minor corrections to adding 1 to date

--- src/main/java/com/squidward/beans/BurnDownData.java
+++ src/main/java/com/squidward/beans/BurnDownData.java
@@ -3,11 +3,12 @@ package com.squidward.beans;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
 import java.util.Map;
 
 @Getter @Setter
 public class BurnDownData {
 
     private int sum;
-    private Map<String, Integer> dateDifficultyMap;
+    private Map<Date, Integer> dateDifficultyMap;
 }
