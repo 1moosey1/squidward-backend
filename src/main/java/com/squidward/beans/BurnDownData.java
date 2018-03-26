51e8b4b1fa6b436847c4bdb0dedfe941f735c3b2
1moosey1 <jgonzalez498@gmail.com>
2018-03-25 21:17:22 -0400

    Changed burndown map to use strings instead of dates

--- src/main/java/com/squidward/beans/BurnDownData.java
+++ src/main/java/com/squidward/beans/BurnDownData.java
@@ -3,12 +3,11 @@ package com.squidward.beans;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
 import java.util.Map;
 
 @Getter @Setter
 public class BurnDownData {
 
     private int sum;
-    private Map<Date, Integer> dateDifficultyMap;
+    private Map<String, Integer> dateDifficultyMap;
 }
